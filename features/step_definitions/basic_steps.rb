a9d40db77fbac2d903807781d43ac2a2e42540a7
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-05 14:22:23 +0200

    renames steps files to basic and assertions

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -11,3 +11,11 @@ Given("the following articles exists") do |table|
         Article.create!(article)
     end
 end
+
+Given("I click {string}") do |element|
+    click_on element
+  end
+
+  When("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
