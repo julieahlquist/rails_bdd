c3618f3f5ab1c11a9367fe8ab7afa3dc2a647036
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-07 15:40:31 +0200

    adds show as action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index
-  resources :articles, only: [:new, :create, :index]
+  resources :articles, only: [:new, :create, :index, :show]
     resources :comments
 end
