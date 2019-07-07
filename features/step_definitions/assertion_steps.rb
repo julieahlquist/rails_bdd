6d78dfe5824fdf967017d9a92dd904d911dca6a2
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-07 16:20:58 +0200

    adds comment form, refactors comment feature again and attemps to add assertion step

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -6,3 +6,8 @@ end
 Then("I should see {string}") do |content|
     expect(page).to have_content content
 end
+
+Then("I should be on {string}") do |page|
+    article = Article.find_by(title: page)
+    expect(current_path).to eq article_path(article)
+end
