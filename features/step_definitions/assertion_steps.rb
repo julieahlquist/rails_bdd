4bf0b5b9e14b15cc4cffb8230af6e17d4caae8ec
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-08 09:31:45 +0200

    cleans out some comments and modifies submit comment button

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -6,8 +6,3 @@ end
 Then("I should see {string}") do |text|
     expect(page).to have_content text
 end
-
-# Then("I should be on {string}") do |page|
-#     article = Article.find_by(title: page)
-#     expect(current_path).to eq article_path(article)
-# end
