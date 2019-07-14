aafcc327c5a0549c6087b6faa944dec7d7d1f453
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-14 22:03:22 +0200

    adds resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root controller: :articles, action: :index
-  resources :articles, only: [:new, :create, :index, :show]
+  resources :articles, only: [:new, :create, :index, :show, :edit, :destroy]
     resources :comments
 end
